d578a76476d81871f7e613459e0896934cc77b56
bishalbera <bishalbera2@gmail.com>
2025-05-06 17:23:13 +0530

    feat: made cart service skeleton

---
+++ apps/cart-service/internal/server/server.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"cart-service/internal/database"
+)
+
+type FiberServer struct {
+	*fiber.App
+
+	db database.Service
+}
+
+func New() *FiberServer {
+	server := &FiberServer{
+		App: fiber.New(fiber.Config{
+			ServerHeader: "cart-service",
+			AppName:      "cart-service",
+		}),
+
+		db: database.New(),
+	}
+
+	return server
+}
